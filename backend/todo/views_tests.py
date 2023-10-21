00ad1e566870164eab293b559c3f0cddd52b6514
ryrichard <66878091+ryrichard@users.noreply.github.com>
2023-10-21 14:44:00 -0400

    Changes from test

--- backend/todo/views_tests.py
+++ backend/todo/views_tests.py
@@ -1,18 +1,57 @@
-from django.urls import reverse
-from rest_framework.test import APITestCase
-from rest_framework import status
+from django.test import TestCase
+from rest_framework.test import APIClient
 from .models import Todo
-from .serializers import TodoSerializer
 
-
-class TodoViewIntegrationTest(APITestCase):
+class TodoViewIntegrationTest(TestCase):
     def setUp(self):
-        # Create test data
-        self.todo = Todo.objects.create(title="Test Task")
+        # Create some sample data for testing
+        self.client = APIClient()
+        self.todo_data = {
+            'title': 'Test Task',
+            'description': 'This is a test task.',
+            'completed': False
+        }
+        self.todo = Todo.objects.create(**self.todo_data)
 
-    def test_get_todo_list(self):
-        url = reverse('/tasks/')  # Assuming you have a URL named 'todo-list' for your viewset
-        response = self.client.get(url)
+    # checking the existance of created todo
+    def test_check_existance_of_new_todos(self):
+        response = self.client.get('/api/tasks/')
+        # print(response)
+        assert response.status_code == 200
+        assert len(response.data) == 1
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)  # Assuming it returns a list with one item
+    # checking if creating a new todo will give 201
+    def test_check_created_todo(self):
+        new_todo_data = {
+            'title': 'New Task',
+            'description': 'A new task.',
+            'completed': True
+        }
+        response = self.client.post('/api/tasks/', new_todo_data, format='json')
+        # print(response)
+        assert response.status_code == 201
+
+    # problem creating incomplete todo object
+    def test_problem_with_incomplete_todo(self):
+        new_todo_problem = {
+            'description': "A problem",
+            'completed': False
+        }
+        response = self.client.post('/api/tasks/', new_todo_problem, format='json')
+        assert response.status_code == 400
+
+    # def test_update_todo(self):
+    #     updated_data = {
+    #         'title': 'Updated Task',
+    #         'completed': True
+    #     }
+    #     response = self.client.put(f'/api/todos/{self.todo.id}/', updated_data, format='json')
+    #     self.assertEqual(response.status_code, 200)
+    #     self.todo.refresh_from_db()  # Reload the object from the database
+    #     self.assertEqual(self.todo.title, 'Updated Task')
+    #     self.assertEqual(self.todo.completed, True)
+
+    # def test_delete_todo(self):
+    #     response = self.client.delete(f'/api/todos/{self.todo.id}/')
+    #     self.assertEqual(response.status_code, 204)
+    #     self.assertFalse(Todo.objects.filter(id=self.todo.id).exists())
