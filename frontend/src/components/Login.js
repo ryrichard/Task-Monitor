9ddcc63c34b666477c4def7bf9c4a928b0de5ff8
Maoxi16 <mx1351585774@gmail.com>
2023-12-02 10:00:40 -0500

    update frontend

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,26 +1,43 @@
 import React from "react";
-import './Login.css';
+import { useState } from "react";
+import "./Login.css";
 
 export default function Login() {
- return (
-   <div className="login">
-     <h1>Login</h1>
-     <form>
-       <div className="form-group">
-         <label className="form-label">Email Address</label>
-         <input type="email" placeholder="Enter your email" />
-       </div>
-    
-       <div className="form-group">
-         <label className="form-label">Password</label>
-         <input type="password" placeholder="Enter your password" />
-       </div>
+  const [email, setemail] = useState("");
+  const [password, setPassword] = useState("");
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // need to connect to DB
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="login">
+        <h1>Login</h1>
+        <form>
+          <div className="form-group">
+            <label className="form-label">Email Address</label>
+            <input
+              type="email"
+              onChange={(e) => setemail(e.target.value)}
+              placeholder="Enter your email"
+            />
+          </div>
 
-       <button type='Submit' className='button'>
-         Submit
-       </button>
-     </form>
-   </div>
- );
+          <div className="form-group">
+            <label className="form-label">Password</label>
+            <input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter your password"
+            />
+          </div>
+
+          <button type="Submit" className="button">
+            Submit
+          </button>
+        </form>
+      </div>
+    </form>
+  );
 }
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,31 +1,59 @@
 import React from "react";
-import './Register.css';
+import { useState } from "react";
+import "./Register.css";
 
 export default function Register() {
-   return (
-     <div className="register">
-       <h1>Sign Up</h1>
-       <form>
-       <div className="form-group">
-           <label className="form-label">Name</label>
-           <input type="name" placeholder="Enter your full name" />
-           </div>
-           <div className="form-group">
-           <label className="form-label">Email Address</label>
-           <input type="email" placeholder="Enter your email" />
-           </div>
-           <div className="form-group">
-           <label className="form-label">Password</label>
-           <input type="password" placeholder="Enter your password" />
-           </div>
-           <div className="form-group">
-           <label className="form-label">Confirm Password</label>
-           <input type="password" placeholder="Retype your password" />
-           </div>
-           <button type='Submit' className='button'>
-             Register
-           </button>
-         </form>
-   </div>
-   );
- }
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setemail] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // need to connect to DB
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="register">
+        <h1>Sign Up</h1>
+        <form>
+          <div className="form-group">
+            <label className="form-label">Name</label>
+            <input
+              type="name"
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Enter your full name"
+            />
+          </div>
+          <div className="form-group">
+            <label className="form-label">Email Address</label>
+            <input
+              type="email"
+              onChange={(e) => setemail(e.target.value)}
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="form-group">
+            <label className="form-label">Password</label>
+            <input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter your password"
+            />
+          </div>
+          <div className="form-group">
+            <label className="form-label">Confirm Password</label>
+            <input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Retype your password"
+            />
+          </div>
+          <button type="Submit" className="button">
+            Register
+          </button>
+        </form>
+      </div>
+    </form>
+  );
+}
